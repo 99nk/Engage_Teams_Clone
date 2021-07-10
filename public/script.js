f75e9c11fd5688e31074c459ac07d9b35e69939e
Niharika Kumar <99.niharikakumar@gmail.com>
2021-07-11 00:36:37 +0530

    bug removed

--- public/script.js
+++ public/script.js
@@ -136,8 +136,8 @@ send.addEventListener("click", (e) => {
       obj.toString();
       let str=obj;
       let l=str.length;
-      //let ans=str.substr(26,l);
-      ans='1234';
+      let ans=str.substr(35,l);
+      //ans='1234';
       let dis='Groups/'+ans+'/';
       firebase.database().ref(dis+'1234').set(msgData,(error)=>{
         if(error)
